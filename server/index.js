e9146b0b82416844141342f4d00d7d57b7fe3d6a
Andras Toth <andras.toth@risingstack.com>
2016-03-07 17:52:11 +0100

    refactor(server): clean up index.js

--- server/index.js
+++ server/index.js
@@ -1,25 +1,33 @@
 'use strict';
-// Load environment vars and .env file into process.env.
+
+// Load .env file into process.env
 require('dotenv').load();
 
-var path = require('path');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var express = require('express');
-var favicon = require('serve-favicon');
-var _ = require('lodash');
+const path = require('path');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const favicon = require('serve-favicon');
 
-var config = require('./config');
+const config = require('./config');
+
+const Database = require('./database');
+const Observer = require('./observer');
+
+const api = require('./api');
+const proxy = require('./proxy');
+const views = require('./views');
+const health = require('./health');
 
 // This attempts to send over our devices to Salesforce. It
 // does nothing if:
 // 1. The data has already been sent to Salesforce
 // 2. There are no Salesforce credentials set up in the environment
 // Refer to the README to figure out how to set up 2
-var attemptSalesforce = require('./attempt-salesforce');
-attemptSalesforce();
+require('./attempt-salesforce')();
 
-var mqttConfig = {
+const mqttConfig = {
   host: `mqtt://${config.account.brokerHost}`,
   clientId: config.account.device.mqtt.username,
   username: config.account.device.mqtt.username,
@@ -27,29 +35,25 @@ var mqttConfig = {
   accountId: config.account.accountId
 };
 
-var app = express();
-app.set('port', process.env.PORT || 5000);
+const app = express();
 
-var Database = require('./database');
-var database = new Database({
-  databaseURL: process.env.DATABASE_URL
+const database = new Database({
+  databaseURL: config.databaseURL
 });
 app.set('db', database);
 
-var Observer = require('./observer');
-var observer = new Observer(database, mqttConfig);
+const observer = new Observer(database, mqttConfig);
 app.set('observer', observer);
 
-var parsers = express();
-parsers.use(bodyParser.json());
-parsers.use(bodyParser.urlencoded({
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
   extended: false
 }));
-parsers.use(cookieParser());
+app.use(cookieParser());
 
-var configLoading = true;
+let configLoading = true;
 database.selectApplicationConfig(config.account.accountId).then(function(data) {
-  var appConfig = data[0];
+  const appConfig = data[0];
   _.merge(config, {
     account: {
       organizationId: appConfig.organization.id,
@@ -70,12 +74,13 @@ database.selectApplicationConfig(config.account.accountId).then(function(data) {
   });
 
   configLoading = false;
-}).catch(function(error) {
-  console.log(error);
+}).catch((error) => {
+  console.error(error);
+  throw new Error('Can not get application config');
 });
 
 // Show "loading" page until Blueprint data has been fetched.
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   if (!configLoading) {
     return next();
   }
@@ -83,22 +88,18 @@ app.use(function(req, res, next) {
 });
 
 // Api routes
-var api = require('./api');
-app.use('/api', parsers, api());
+app.use('/api', api());
 
 // View routes
-var views = require('./views');
 app.use('/', views({
   config: config,
 }));
 
 // Health checker route
-var health = require('./health');
-app.use('/diag/selftest', parsers, health());
+app.use('/diag/selftest', health());
 
 // Proxy route
-var proxy = require('./proxy');
-app.use('/api/proxy', parsers, proxy({
+app.use('/api/proxy', proxy({
   whitelist: [
     'http://www.airnowapi.org/aq/data',
     'https://timeseries.demo.xively.com/api/v4/data/xi/blue/v1',
@@ -109,16 +110,18 @@ app.use('/api/proxy', parsers, proxy({
 // Serve up favicon
 app.use(favicon(path.join(__dirname, '/favicon.ico')));
 
-// Page not found.
-app.use(function(req, res) {
+// Page not found
+app.use((req, res) => {
   res.status(404).render('404');
 });
 
-// Initialize the view engine.
+// Initialize the view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // Start server up
-var server = app.listen(app.get('port'), function() {
+const server = app.listen(config.server.port, () => {
   console.log('Node server listening on port', server.address().port);
 });
+
+module.exports = server;
