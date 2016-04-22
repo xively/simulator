e90b0fb3d9a24d5b72c796ab9e89bbf0e8e47748
David Kiss <david@risingstack.com>
2016-04-22 12:28:10 +0200

    fix(client): redirect to demo mode by default

--- client/app/app.config.js
+++ client/app/app.config.js
@@ -7,7 +7,15 @@ function appConfig ($compileProvider, $logProvider, $provide, $urlRouterProvider
   $compileProvider.debugInfoEnabled(isProduction)
   $logProvider.debugEnabled(!isProduction)
 
-  $urlRouterProvider.otherwise('devices')
+  $urlRouterProvider.otherwise(($injector) => {
+    const blueprintService = $injector.get('blueprintService')
+    const $state = $injector.get('$state')
+
+    blueprintService.getV1('devices', { pageSize: 2 }).then((response) => {
+      const id = response.data.devices.results[1].id
+      $state.go('devices.device-demo', { id, header: 0 })
+    })
+  })
 }
 
 module.exports = appConfig
