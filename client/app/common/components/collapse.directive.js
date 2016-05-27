de849310b757018ac530ea66e09e3d769b779c81
I <andras.toth@risingstack.com>
2016-05-27 16:47:45 +0200

    refactor(app): collapse

--- client/app/common/components/collapse.directive.js
+++ client/app/common/components/collapse.directive.js
@@ -3,19 +3,10 @@ function collapse ($timeout) {
   return {
     restrict: 'A',
     scope: {
-      isOpen: '<',
-      triggerResize: '<'
+      isOpen: '<'
     },
     link (scope, element, attrs) {
       const panel = element[0]
-
-      scope.$watch('triggerResize', function (triggerResize) {
-        panel.style.height = 'auto'
-
-        $timeout(function () {
-          panel.style.height = `${panel.offsetHeight}px`
-        }, 100)
-      })
 
       scope.$watch('isOpen', function (isOpen) {
         if (isOpen) {
